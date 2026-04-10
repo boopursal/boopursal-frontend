60921ac3e57341d147b8c07c8e5415b9f079969d
boostfek-droid <boostfek@gmail.com>
2026-04-10 12:11:44 +0100

    fix(confirm): force redirection to registration steps after success

--- src/app/main/inscription/ConfirmPage.js
+++ src/app/main/inscription/ConfirmPage.js
@@ -8,6 +8,7 @@ import _ from '@lodash';
 
 function ConfirmPage(props) {
     const dispatch = useDispatch();
+    const user = useSelector(({ auth }) => auth.user);
     const login = useSelector(({ auth }) => auth.login);
     const params = props.match.params;
     const { confirmationToken } = params;
@@ -19,29 +20,38 @@ function ConfirmPage(props) {
     }, [dispatch, confirmationToken]);
 
     useEffect(() => {
-        if (login.error && (login.error.confirmationToken || login.error.Erreur))
+        if (user && user.role) {
+            if (user.role === 'ROLE_FOURNISSEUR_PRE') {
+                props.history.push('/register/fournisseur');
+            } else if (user.role === 'ROLE_ACHETEUR_PRE') {
+                props.history.push('/register/acheteur');
+            } else if (user.role === 'ROLE_FOURNISSEUR') {
+                props.history.push('/boopursal/fournisseur/dashboard');
+            } else if (user.role === 'ROLE_ACHETEUR') {
+                props.history.push('/boopursal/acheteur/dashboard');
+            } else {
+                props.history.push('/');
+            }
+        }
+    }, [user, props.history]);
+
+    useEffect(() => {
+        if (login.error && (login.error.confirmationToken || login.error.Erreur)) {
             dispatch(
                 Actions.showMessage({
                     message: _.map(login.error, function (value, key) {
                         return login.error.confirmationToken ? 'Le code d\'activation est incorrect' : value;
                     }),
-                    autoHideDuration: 6000,//ms
-                    anchorOrigin: {
-                        vertical: 'top',//top bottom
-                        horizontal: 'right'//left center right
-                    },
-                    variant: 'error'//success error info warning null
+                    autoHideDuration: 6000,
+                    variant: 'error'
                 }));
-
-        const timer = setTimeout(() => {
-            props.history.push({
-                pathname: '/login'
-            })
-        }, 3000);
-        return () => clearTimeout(timer);
-
-
-    }, [dispatch, login.error]);
+            
+            const timer = setTimeout(() => {
+                props.history.push('/login');
+            }, 3000);
+            return () => clearTimeout(timer);
+        }
+    }, [dispatch, login.error, props.history]);
 
 
     return (
