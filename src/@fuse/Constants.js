0d6c92c394c5e7131653a5bf9001457fc762da66
boostfek-droid <boostfek@gmail.com>
2026-06-01 21:52:36 +0100

    fix(constants): add trailing slash to URL_SITE to fix image URL concatenation

--- src/@fuse/Constants.js
+++ src/@fuse/Constants.js
@@ -1,4 +1,4 @@
-export const URL_SITE = (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') ? "http://localhost:3002" : "https://boopursal-backend.vercel.app";
+export const URL_SITE = (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') ? "http://localhost:3002/" : "https://boopursal-backend.vercel.app/";
 export const FRONT_URL = "https://www.boopursal.com";
 export const LOCAL_CURRENCY = "MAD";
 export const LOCAL_TVA = 0.2;
