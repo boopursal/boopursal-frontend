6b36849de5d3d046d8ce3444f81495f1ab5da2bc
boostfek-droid <boostfek@gmail.com>
2026-08-13 12:43:47 +0100

    Content: Update hero title text on the homepage

--- src/app/main/portail/index/Index.js
+++ src/app/main/portail/index/Index.js
@@ -1,4 +1,4 @@
-﻿import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   Icon,
   Typography,
@@ -755,7 +755,7 @@ function Index(props) {
                 <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"><polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon></svg>
                 LA RÉFÉRENCE B2B
               </div>
-              <h1 className={classes.heroTitle} dangerouslySetInnerHTML={{ __html: t('portail.heroTitle', 'Connectez acheteurs et fournisseurs <br/> sur <span style="color: #274b99;">une seule plateforme</span>') }} />
+              <h1 className={classes.heroTitle} dangerouslySetInnerHTML={{ __html: t('portail.heroTitle', 'Acheteur & Fournisseurs: connectez-vous <br/> aux <span style="color: #274b99;">bonnes opportunités</span>') }} />
             </div>
           </FuseAnimate>
 
